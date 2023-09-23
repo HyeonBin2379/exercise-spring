cc0e669dea14852da2b94cc886574f86937f0b70
hyunbinny@naver.com <hyunbinny@naver.com>
2023-09-23 17:00:02 +0900

    Initial commit

---
+++ src/main/java/exercise/exercisespring/ExerciseSpringApplication.java
@@ -0,0 +1,13 @@
+package exercise.exercisespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciseSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciseSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/exercise/exercisespring/ExerciseSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package exercise.exercisespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciseSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
