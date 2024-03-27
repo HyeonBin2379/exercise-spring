8b8d0d6e7a6737418fec127050984be389a1116a
Hyeonbin2379 <hyeonbin2379@gmail.com>
2024-03-27 18:47:04 +0900

    feat : 비밀번호 변경 기능 구현 로그인하지 않은 상태에서의 비밀번호 변경 기능 접근 방지용 Interceptor 추가, 변경한 비밀번호에 대한 Controller 범위 유효성 검사 추가

---
+++ src/main/java/controller/ChangePwdCommand.java
@@ -0,0 +1,28 @@
+package controller;
+
+import jakarta.validation.constraints.Size;
+
+public class ChangePwdCommand {
+
+    @Size(min=6)
+    private String currentPassword;
+
+    @Size(min=6)
+    private String newPassword;
+
+    public String getCurrentPassword() {
+        return currentPassword;
+    }
+
+    public void setCurrentPassword(String currentPassword) {
+        this.currentPassword = currentPassword;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
+}
