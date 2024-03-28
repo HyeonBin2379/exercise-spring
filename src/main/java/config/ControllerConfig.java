27cd61627e39e617fa22d6bd00c7a4e72f7b965e
Hyeonbin2379 <hyeonbin2379@gmail.com>
2024-03-28 19:14:18 +0900

    feat : 가입일에 따른 회원 조회 및 회원 정보 열람 기능 추가

--- src/main/java/config/ControllerConfig.java
+++ src/main/java/config/ControllerConfig.java
@@ -3,6 +3,8 @@ package config;
 import controller.ChangePwdController;
 import controller.LoginController;
 import controller.LogoutController;
+import controller.MemberDetailController;
+import controller.MemberListController;
 import controller.RegisterController;
 import controller.SurveyController;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import spring.AuthService;
 import spring.ChangePasswordService;
+import spring.MemberDao;
 import spring.MemberRegisterService;
 
 @Configuration
@@ -19,9 +22,10 @@ public class ControllerConfig {
     private MemberRegisterService memberRegSvc;
     @Autowired
     private AuthService authService;
-
     @Autowired
     private ChangePasswordService changePasswordService;
+    @Autowired
+    private MemberDao memberDao;
 
     @Bean
     public RegisterController registerController() {
@@ -51,6 +55,20 @@ public class ControllerConfig {
     public ChangePwdController changePwdController() {
         ChangePwdController controller = new ChangePwdController();
         controller.setChangePasswordService(changePasswordService);
+        return controller;
+    }
+
+    @Bean
+    public MemberListController memberListController() {
+        MemberListController controller = new MemberListController();
+        controller.setMemberDao(memberDao);
+        return controller;
+    }
+
+    @Bean
+    public MemberDetailController memberDetailController() {
+        MemberDetailController controller = new MemberDetailController();
+        controller.setMemberDao(memberDao);
         return controller;
     }
 }
---
+++ src/main/webapp/WEB-INF/view/member/memberDetail.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=utf-8" pageEncoding="utf-8"%>
+<%@ taglib tagdir="/WEB-INF/tags" prefix="tf" %>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>회원 정보</title>
+    </head>
+    <body>
+        <p>아이디: ${member.id}</p>
+        <p>이메일: ${member.email}</p>
+        <p>이름: ${member.name}</p>
+        <p>가입일: <tf:formatDateTime value="${member.registerDateTime}"
+            pattern="yyyy-MM-dd HH:mm" /></p>
+    </body>
+</html>
